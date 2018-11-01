f74bbedb5b7b331300daa3b838f4af07d70ff370
busayo08 <43090736+busayo08@users.noreply.github.com>
2018-11-01 03:17:06 +0100

    Update quiz2.php

--- quiz2.php
+++ quiz2.php
@@ -2,11 +2,10 @@
 $one="49";
 $two="300";
 $three="13";
-
 $multiply = $one * $three;
 $substract = $one - $two;
 $divide = $three/$two;
-echo "The value of 49 multipied by 13 is"." ". $multiply."<br>";
-echo "if 49 is substracted from 300 we have"." ". $substract."<br>";
-echo "division: 13 divided by 300 equals"." ". $divide
+echo "The value of"." ".$one." multipied by "." ".$three." is"." ". $multiply."<br>";
+echo "if "." ".$one." is substracted from "." ".$two." we have"." ". $substract."<br>";
+echo "division: "." ".$one." divided by "." ".$two." equals"." ". $divide
 ?>
