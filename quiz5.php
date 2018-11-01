3cb971c46f43935fe9ae35fe7c89517e7d2c4428
busayo08 <43090736+busayo08@users.noreply.github.com>
2018-11-01 03:14:00 +0100

    Update quiz5.php

--- quiz5.php
+++ quiz5.php
@@ -1,7 +1,8 @@
 <?php
-$people = array("Busayo"=>"Football", "Damilola"=>"Basketball", "Jerry"=>"Tennis");
-foreach($people as $name => $hobby) {
-    echo "My name is"." ". $name . ", I love"." " . $hobby;
-    echo "<br>";
+$people = array("Busayo", "Damilola", "Jerry");
+$hobby = array("Football", "Basketball", "Tennis");
+{
+    foreach (array_combine($people, $hobby) as $name => $hb)
+    echo "My name is"." ". $name . ", I love"." " . $hb ."<br>";    
 }
 ?>
