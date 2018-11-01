736310821095fcf65b9db6630fbc056613e34b2d
busayo08 <43090736+busayo08@users.noreply.github.com>
2018-11-01 03:14:50 +0100

    Update quiz4.php

--- quiz4.php
+++ quiz4.php
@@ -1,20 +1,12 @@
 <?php
 for( $a=100; $a<=150; $a++ )
-
-{
-
-echo $a;
-
-echo "<br>";
-
+{ 
+    echo $a;
+    echo "<br>";
 }
 for( $b=0; $b<=50; $b++ )
-
-{
-
-echo $b;
-
-echo "<br>";
-
+if ($b % 2 == 0) {
+    echo $b;
+    echo "<br>";
 }
 ?>
