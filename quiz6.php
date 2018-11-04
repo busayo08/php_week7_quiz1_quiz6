969ca0cbe0ee256be86203f02234fb9f4598a1ba
busayo08 <43090736+busayo08@users.noreply.github.com>
2018-11-04 18:01:49 +0100

    Update quiz6.php

--- quiz6.php
+++ quiz6.php
@@ -1,7 +1,11 @@
 <?php
 $hundred = 100; $ten = 10;
-function myTest() { global $hundred, $ten;}
+function myTest() 
+{ 
+    global $hundred, $ten;
+    echo $hundred + $ten."<br>"; 
+    echo $hundred * $ten."<br>";
+}
 myTest();
-echo $hundred + $ten."<br>"; 
-echo $hundred * $ten."<br>";
+
 ?>
