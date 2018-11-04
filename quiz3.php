21148ec00443d5835eb5604ebcf4c35d1ca59ef4
busayo08 <43090736+busayo08@users.noreply.github.com>
2018-11-04 18:04:57 +0100

    Update quiz3.php

--- quiz3.php
+++ quiz3.php
@@ -1,12 +1,12 @@
 <?php 
-$temperature = "24";
-if ($temperature < 20 && $temperature = "20") {    
+$temperature = "20";
+if ($temperature <=20) {    
     echo "It is really cold today"; 
     } elseif ($temperature > 20 && $temperature < 30) {  
      echo "The weather is just perfect"; 
-    } elseif ($temperature < 30 && $temperature = 30 && $temperature < 40) {  
+    } elseif ($temperature >= 30 && $temperature < 40) {  
     echo "It's so hot today"; 
-   } elseif ($temperature > 40 && $temperature =40){  
+   } elseif ($temperature >=40){  
     echo "Am I in the Sahara Desert!"; 
 } 
 ?>
